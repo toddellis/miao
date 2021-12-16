f73020b85ccc8aa96d46b572778e5e1583d5a57a
Todd Ellis <Todd.Ellis@utas.edu.au>
2021-12-16 11:36:43 +1100

    Expaned create_project

--- R/create_project.R
+++ R/create_project.R
@@ -9,18 +9,28 @@
 #' create_project()
 #'}
 
-create_project <- function() {
+create_project <- function(quiet = FALSE) {
 
   .dir <- getwd()
 
   suppressWarnings(dir.create(paste0(.dir, '/data/')))
   suppressWarnings(dir.create(paste0(.dir, '/data/input/')))
-  suppressWarnings(dir.create(paste0(.dir, '/data/output')))
+  suppressWarnings(dir.create(paste0(.dir, '/data/output/')))
+  suppressWarnings(dir.create(paste0(.dir, '/data/scratch/')))
   suppressWarnings(dir.create(paste0(.dir, '/documents/')))
   suppressWarnings(dir.create(paste0(.dir, '/documents/literature/')))
+  suppressWarnings(dir.create(paste0(.dir, '/documents/pptx/')))
+  suppressWarnings(dir.create(paste0(.dir, '/documents/reports/')))
+  suppressWarnings(dir.create(paste0(.dir, '/documents/scratch/')))
   suppressWarnings(dir.create(paste0(.dir, '/figures/')))
+  suppressWarnings(dir.create(paste0(.dir, '/figures/scratch/')))
+  suppressWarnings(dir.create(paste0(.dir, '/scripts/')))
+  suppressWarnings(dir.create(paste0(.dir, '/scripts/scratch/')))
   suppressWarnings(dir.create(paste0(.dir, '/tables/')))
+  suppressWarnings(dir.create(paste0(.dir, '/tables/scratch/')))
 
-  print(paste0('Project directories created in \'', .dir, '/\'.'))
+  if (!quiet) {
+    print(paste0('Project directories created in \'', .dir, '/\'.'))
+  }
 
 }
