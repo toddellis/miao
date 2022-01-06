c9fb99ecc1daaf55c486b3713d2c711247fbfd01
Todd Ellis <Todd.Ellis@utas.edu.au>
2022-01-06 15:34:56 +1100

    Added tidy_gam

---
+++ R/tidy_gam.R
@@ -0,0 +1,51 @@
+#' tidy_gam
+#'
+#' Wrapped for 2x broom::tidy as applied to a GAM object, pulling both parametric and smooth terms.
+#'
+#' @param x GAM model object
+#' @param conf.level Confidence level for pulling confidence intervals from parametric terms.
+#'
+#' @return Dataframe with model/term metrics.
+#' @export
+#'
+#' @examples
+#'
+#'
+
+
+tidy_gam <- function(x,
+                     conf.level = 0.95) {
+  .model <-
+    x
+  .conf.level <-
+    conf.level
+
+  .parametric <-
+    .model |>
+    broom::tidy(parametric = TRUE,
+                conf.int = TRUE,
+                conf.level = .conf.level) |>
+    dplyr::mutate(term.type = "Parametric")
+
+  .nonparametric <-
+    .model |>
+    broom::tidy(parametric = FALSE) |>
+    dplyr::mutate(term.type = "Smooth")
+
+  output <-
+    .parametric |>
+    dplyr::bind_rows(.nonparametric) |>
+    dplyr::select(term,
+                  term.type,
+                  estimate,
+                  std.error,
+                  edf,
+                  ref.df,
+                  statistic,
+                  p.value,
+                  conf.low,
+                  conf.high)
+
+  return(output)
+
+}
