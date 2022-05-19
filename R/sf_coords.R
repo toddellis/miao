58cd6155f3e8dcd4e9fd76466d82a5a4f7b5c842
Todd Ellis <Todd.Ellis@utas.edu.au>
2022-05-19 15:58:54 +1000

    Added sf_coords

---
+++ R/sf_coords.R
@@ -0,0 +1,21 @@
+#' sf_coords
+#'
+#' Extract x-y coordinate values from sf features.
+#'
+#' @param x sf feature type.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' }
+#'
+
+sf_coords <- function(x) {
+  x |>
+    dplyr::bind_cols(x |>
+                       sf::st_point_on_surface() |>
+                       sf::st_coordinates()) |>
+    dplyr::rename(x = X, y = y)
+}
